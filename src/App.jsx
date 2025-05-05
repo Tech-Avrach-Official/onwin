5f4008c2c54ee3f269260a5c022ebd0512a5c7fe
Tech-Avrach-Official <techavrach@gmail.com>
2025-05-05 16:21:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./component/Navbar"
+import Home from "./pages/Home/Home"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
+
